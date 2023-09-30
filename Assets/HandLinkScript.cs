0fe5005f04a910f1d7937bef3eb6d36cf549b4e3
Nesathurai <ahnesathurai@gmail.com>
2023-09-29 21:49:45 -0400

    added dictionary, better perf

--- Assets/HandLinkScript.cs
+++ Assets/HandLinkScript.cs
@@ -1,35 +1,50 @@
 using UnityEngine;
+using System.Collections.Generic; 
+
 
 public class BoneRotationCopier : MonoBehaviour
 {
     public GameObject sourceModel; // Reference to the model you want to copy bone rotations from.
     public GameObject targetModel; // Reference to the model you want to copy bone rotations to.
-
-
-    void Update()
+    IDictionary<Transform, Transform> boneMap = new Dictionary<Transform, Transform>();
+    void Start()
     {
-        targetModel.transform.position = sourceModel.transform.position;
-        targetModel.transform.rotation = sourceModel.transform.rotation;
-        
+        boneMap.Add(sourceModel.transform, targetModel.transform);
         // Iterate through each bone in the source model.
         foreach (Transform sourceBone in sourceModel.GetComponentsInChildren<Transform>())
         {
             foreach (Transform targetBone in targetModel.GetComponentsInChildren<Transform>())
             {
                 if(sourceBone.name == targetBone.name){
-                    targetBone.transform.localRotation = sourceBone.transform.localRotation;
+                    boneMap.Add(sourceBone, targetBone);
+                    // targetBone.transform.localRotation = sourceBone.transform.localRotation;
+                    continue;
                 }
             }
-            
-            // Find the corresponding bone in the target model based on the name.
-            // Transform targetBone = sourceBone.transform.Find(sourceBone.name);
-            // Debug.Log(sourceBone.name);
-            // if (targetBone != null)
-            // {
-            //     Debug.Log("FOUND: " + sourceBone.name);
-            //     // Copy the rotation from the source bone to the target bone.
-            //     
-            // }
         }
+    }
+
+    void Update()
+    {
+        // targetModel.transform.position = sourceModel.transform.position;
+        // targetModel.transform.rotation = sourceModel.transform.rotation;
+        foreach(KeyValuePair<Transform, Transform> entry in boneMap)
+        {
+            entry.Value.position = entry.Key.position;
+            entry.Value.rotation = entry.Key.rotation;
+        }
+
+        
+        // Iterate through each bone in the source model.
+        // foreach (Transform sourceBone in sourceModel.GetComponentsInChildren<Transform>())
+        // {
+        //     foreach (Transform targetBone in targetModel.GetComponentsInChildren<Transform>())
+        //     {
+        //         if(sourceBone.name == targetBone.name){
+        //             targetBone.transform.localRotation = sourceBone.transform.localRotation;
+        //             continue;
+        //         }
+        //     }
+        // }
     }
 }
