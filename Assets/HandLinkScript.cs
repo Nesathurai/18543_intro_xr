619419a1a05b5f9ff9a3a8a948d78aee10b9a521
Nesathurai <ahnesathurai@gmail.com>
2023-10-24 11:26:26 -0400

    saving json files

--- Assets/HandLinkScript.cs
+++ Assets/HandLinkScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System;
 using System.Collections.Generic; 
 using System.Collections;
+using System.IO;
 using Newtonsoft.Json;
 
 public class BoneRotationCopier : MonoBehaviour
@@ -11,9 +12,9 @@ public class BoneRotationCopier : MonoBehaviour
     IDictionary<Transform, Transform> boneMap = new Dictionary<Transform, Transform>();
     void Start()
     {
-        IDictionary<string, BoneData> boneData = new Dictionary<string, BoneData>();
+        // IDictionary<string, BoneData> boneData = new Dictionary<string, BoneData>();
         boneMap.Add(sourceModel.transform, targetModel.transform);
-        boneData.Add(sourceModel.name, new BoneData(sourceModel.transform.position, sourceModel.transform.rotation));
+        // boneData.Add(sourceModel.name, new BoneData(sourceModel.transform.position, sourceModel.transform.rotation));
         // Iterate through each bone in the source model.
         foreach (Transform sourceBone in sourceModel.GetComponentsInChildren<Transform>())
         {
@@ -21,16 +22,20 @@ public class BoneRotationCopier : MonoBehaviour
             {
                 if(sourceBone.name == targetBone.name){
                     boneMap.Add(sourceBone, targetBone);
-                    boneData.Add(sourceBone.name, new BoneData(sourceBone.transform.position, sourceBone.transform.rotation));
+                    // boneData.Add(sourceBone.name, new BoneData(sourceBone.transform.position, sourceBone.transform.rotation));
                     // Debug.Log(sourceBone.name);
                     // targetBone.transform.localRotation = sourceBone.transform.localRotation;
                     continue;
                 }
             }
         }
-        string jsonData = JsonConvert.SerializeObject(boneData, new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
-        Debug.Log(jsonData);
-        // SaveData myData;
+        // string jsonData = JsonConvert.SerializeObject(boneData, new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
+        // Debug.Log(jsonData);
+        // string path = @"C:\Users\ahnes\OneDrive\Documents\GitHub\18543_intro_xr\data\boneData.json";
+        // File.WriteAllText(path, jsonData);
+        // string loaded = File.ReadAllText(path); 
+        // var boneData2 = JsonConvert.DeserializeObject<IDictionary<string, BoneData>>(loaded);
+        // Debug.Log(loaded);
     }
 
 
@@ -38,23 +43,22 @@ public class BoneRotationCopier : MonoBehaviour
     {
         // targetModel.transform.position = sourceModel.transform.position;
         // targetModel.transform.rotation = sourceModel.transform.rotation;
+        if(Input.GetKeyDown("f") || Input.GetKey("f")){
+            IDictionary<string, BoneData> boneData = new Dictionary<string, BoneData>();
+            string path = @"C:\Users\ahnes\OneDrive\Documents\GitHub\18543_intro_xr\data\boneData2.json";
+            foreach(KeyValuePair<Transform, Transform> entry in boneMap)
+            {
+                boneData.Add(entry.Value.name, new BoneData(entry.Value.transform.position, entry.Value.transform.rotation));
+            }
+            string jsonData = JsonConvert.SerializeObject(boneData, new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
+            File.WriteAllText(path, jsonData);
+            Debug.Log("SAVED JSON\n"); 
+        }
         foreach(KeyValuePair<Transform, Transform> entry in boneMap)
         {
             entry.Value.position = entry.Key.position;
             entry.Value.rotation = entry.Key.rotation;
         }
-        
-        // Iterate through each bone in the source model.
-        // foreach (Transform sourceBone in sourceModel.GetComponentsInChildren<Transform>())
-        // {
-        //     foreach (Transform targetBone in targetModel.GetComponentsInChildren<Transform>())
-        //     {
-        //         if(sourceBone.name == targetBone.name){
-        //             targetBone.transform.localRotation = sourceBone.transform.localRotation;
-        //             continue;
-        //         }
-        //     }
-        // }
     }
 }
 
